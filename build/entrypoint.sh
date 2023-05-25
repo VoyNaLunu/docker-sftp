3b193c98d2b8e4be6276ad50a2f087f8cea7f0b1
VoyNaLunu <93346826+VoyNaLunu@users.noreply.github.com>
2023-05-25 11:09:43 +0300

    fix typo

--- build/entrypoint.sh
+++ build/entrypoint.sh
@@ -3,7 +3,7 @@ ssh-keygen -A
 addgroup $GROUP_NAME
 adduser $USER_NAME -G $GROUP_NAME --home /srv/sftp
 echo -e "$USER_PASS\n$USER_PASS" | passwd $USER_NAME
-mkdir -p /srv/sftp \
+mkdir -p /srv/sftp
 chown $USER_NAME:$GROUP_NAME /srv/sftp
 chmod -R 764 /srv/sftp
 sed -i "s/tmp/$USER_NAME/g" /etc/ssh/sshd_config.d/sftp.conf
